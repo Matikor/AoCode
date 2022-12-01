0e55c7bd3d1166e312c7f3c9d3b553970536b42b
Matikor <matikor.fanmail@gmail.com>
2022-12-01 22:43:35 +0100

    01.12-2 done, cleaned up 01.12-1

--- 01.12/01.12-2.cpp
+++ 01.12/01.12-2.cpp
@@ -3,23 +3,27 @@
 
 int main()
 {
-    int calories, maxCalories = 0;
+    int calories, top3[3] = {0, 0, 0}, temp;
     std::ifstream inFile;
     inFile.open("input1.txt");  
          
-
     for(std::string line; std::getline(inFile, line);)
     {  
         if(line == "") 
         {
-            if(calories > maxCalories)
+            for (int i = 0; i < 3; i++)
             {
-                maxCalories = calories;
+                if(top3[i] < calories)
+                {
+                    temp = top3[i];
+                    top3[i] = calories;
+                    calories = temp;
+                }
             }
             calories = 0;
             continue;
         }
         calories += std::stoi(line);
     }
-    std::cout << maxCalories;
+    std::cout << top3[0] + top3[1] + top3[2];;
 }
