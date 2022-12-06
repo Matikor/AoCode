6f19e33b0476b37cbb5272733f04a3e1b0872287
Matikor <mateusz.peczkowski@tm1.edu.pl>
2022-12-06 19:10:26 +0100

    03.12-1 corrected

--- 03.12/03.12-2.cpp
+++ 03.12/03.12-2.cpp
@@ -15,21 +15,28 @@ int main()
     for(char lwalpCh = 'A'; lwalpCh <= 'Z'; lwalpCh++)
 	{prio[i] = lwalpCh; i++;}
     
-    START:for(std::string line; std::getline(inFile, line);)
+    for(std::string line; std::getline(inFile, line);)
     {
+        counter++;
         n = line.length();
-        char array[n], compartment1[n/2], compartment2[n/2];
+        if(counter%3 == 1)
+        {
+            char compartment1[n];
+        }
+        else if (counter%3 == 2){}
+        else if (counter%3 == 0){}
+        char array[n+1], compartment1[(n+1)/2], compartment2[(n+1)/2];
         strcpy(array, line.c_str());
         
-        for (int x = 0; x < n/2; x++)
+        for (int x = 0; x < (n+1)/2; x++)
         {
         compartment1[x] = array[x];
-        compartment2[x] = array[x+n/2];
+        compartment2[x] = array[x+(n+1)/2];
         }
         
-        for(int x = 0; x < n/2; x++)
+        for(int x = 0; x < (n+1)/2; x++)
         {
-            for(int y = 0; y < n/2; y++)
+            for(int y = 0; y < (n+1)/2; y++)
             {
                 if(compartment1[x] == compartment2[y])
                 {
@@ -39,7 +46,6 @@ int main()
                         {
                             prioSum += z;
                             counter++;
-                            goto START;
                         }
                     }
                 } 
