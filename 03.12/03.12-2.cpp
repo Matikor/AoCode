1607d5e2aeeb05b8d0dd36ef51edbb29180519c3
Matikor <mateusz.peczkowski@tm1.edu.pl>
2022-12-09 16:30:09 +0100

    AoCode 03.12-2 done

--- 03.12/03.12-2.cpp
+++ 03.12/03.12-2.cpp
@@ -1,57 +1,56 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include <typeinfo>
+#include <string>
+#include <vector>
 
 int main()
 {
     std::ifstream inFile;
     inFile.open("input.txt");
+    int i = 1, prioSum = 0, counter, n1, n2, n3;
     char prio[52];
-    int i = 1, n, prioSum = 0, counter = 0;
 
     for(char lwalpCh = 'a'; lwalpCh <= 'z'; lwalpCh++)
 	{prio[i] = lwalpCh; i++;}
     for(char lwalpCh = 'A'; lwalpCh <= 'Z'; lwalpCh++)
 	{prio[i] = lwalpCh; i++;}
+    std::vector<char> compartment1, compartment2, compartment3;
     
-    for(std::string line; std::getline(inFile, line);)
+    START:for(std::string line; std::getline(inFile, line);)
     {
         counter++;
-        n = line.length();
-        if(counter%3 == 1)
+        if (counter%3 == 1){std::copy(line.begin(), line.end(), std::back_inserter(compartment1)); n1 = line.length();}
+        if (counter%3 == 2){std::copy(line.begin(), line.end(), std::back_inserter(compartment2)); n2 = line.length();}
+        if (counter%3 == 0)
         {
-            char compartment1[n];
-        }
-        else if (counter%3 == 2){}
-        else if (counter%3 == 0){}
-        char array[n+1], compartment1[(n+1)/2], compartment2[(n+1)/2];
-        strcpy(array, line.c_str());
-        
-        for (int x = 0; x < (n+1)/2; x++)
-        {
-        compartment1[x] = array[x];
-        compartment2[x] = array[x+(n+1)/2];
-        }
-        
-        for(int x = 0; x < (n+1)/2; x++)
-        {
-            for(int y = 0; y < (n+1)/2; y++)
+            std::copy(line.begin(), line.end(), std::back_inserter(compartment3)); 
+            n3 = line.length();
+
+            for(int x = 0; x<n1 ; x++)
             {
-                if(compartment1[x] == compartment2[y])
+                for(int y = 0; y<n2; y++)
                 {
-                    for(int z=1; z<53; z++)
+                    for(int z = 0; z<n3; z++)
                     {
-                        if(prio[z] == compartment1[x])
+                        if(compartment1[x] == compartment2[y] && compartment2[y] == compartment3[z])
                         {
-                            prioSum += z;
-                            counter++;
+                            for(int in=1; in<53; in++)
+                            {
+                                if(prio[in] == compartment1[x])
+                                {
+                                    prioSum += in;
+                                    compartment1.clear();
+                                    compartment2.clear();
+                                    compartment3.clear();
+                                    goto START;
+                                }
+                            }
                         }
                     }
-                } 
+                }
             }
         }
     }
-    std::cout << counter << std::endl;
     std::cout << prioSum;
 }
